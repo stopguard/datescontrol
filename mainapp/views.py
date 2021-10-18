250168770ecb1190a661d355bf321772bd4a5a80
stopguard <stopguard@bk.ru>
2021-10-19 00:02:13 +0500

    stage2 edited check_to logic. prepare format logic in items.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,6 +10,7 @@ def index(request):
 
 def items(request):
     today = datetime.now() + timedelta(hours=5)
+    today = date(today.year, today.month, today.day)
     gived_items = [
         {
             'company': 'ООО СВОС',
@@ -20,8 +21,8 @@ def items(request):
             'inv_num': 'КА1234',
             'check_for': date(2018, 10, 2),
             'test_for': date(2020, 11, 20),
-            'check_to': date(2021, 10, 2),
-            'test_to': date(2021, 11, 20),
+            'check_to': date(2022, 10, 2),
+            'test_to': date(2022, 11, 20),
         },
         {
             'company': 'ООО СВОС',
@@ -66,20 +67,101 @@ def items(request):
             'check_to': date(2021, 12, 2),
         },
     ]
+    city_list = ['Уфа', 'Стерлитамак', 'Нижний Новгород', ]
+    company_list = ['Уфанет', 'СВОС', ]
+    departament_list = ['ОПЮЛ', 'СИ', 'Монтажный отдел']
+    brigadier_list = ['dsfdsfsdfds', 'fdsfdsfsdfdsf', 'ffgggddssss']
+    item_list = ['Электробезопасность 5гр', 'ПТМ', 'Высота 2гр']
     for el in gived_items:
         if el['item_class'] == 'doc' and el['check_to'] > el['test_to']:
             el['check_to'] = el['test_to']
-    city_list = [
-        ''
-    ]
+        delta = (el['check_to'] - today).days
+        if delta > 90:
+            el['status'] = 'OK'
+        elif delta > 30:
+            el['status'] = 'OO'
+        elif delta > 0:
+            el['status'] = 'VV'
+        else:
+            el['status'] = 'XX'
     context = {'page_title': 'список предметов',
                'items': gived_items,
-               'today': today}
+               'cities': city_list,
+               'companies': company_list,
+               'departaments': departament_list,
+               'brigadiers': brigadier_list,
+               'item_types': item_list,
+               'today': today,
+               }
     return render(request, 'mainapp/items.html', context)
 
 
 def departaments(request):
-    context = {'page_title': 'список сотрудников'}
+    today = datetime.now() + timedelta(hours=5)
+    today = date(today.year, today.month, today.day)
+    worker_list = [
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'VV'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'VV'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'VV'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'VV'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'XX'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'XX'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'XX'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'XX'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'OK'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'OK'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'OO'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'OO'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OO'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OO'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'OK'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'OK'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'OK'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'OK'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+        },
+        {
+            'city': 'г. Уфа',
+            'company': {'name': 'ООО СВОС', 'status': 'OK'},
+            'departament': {'name': 'Отдел подключений физических лиц', 'status': 'OK'},
+            'brig_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+            'worker_name': {'name': 'Удинский Дмитрий Игоревич', 'status': 'OK'},
+        },
+    ]
+    city_list = ['Уфа', 'Стерлитамак', 'Нижний Новгород', ]
+    company_list = ['Уфанет', 'СВОС', ]
+    departament_list = ['ОПЮЛ', 'СИ', 'Монтажный отдел']
+    brigadier_list = ['dsfdsfsdfds', 'fdsfdsfsdfdsf', 'ffgggddssss']
+    context = {'page_title': 'список сотрудников',
+               'cities': city_list,
+               'companies': company_list,
+               'departaments': departament_list,
+               'brigadiers': brigadier_list,
+               'workers': worker_list,
+               'today': today,
+               }
     return render(request, 'mainapp/departaments.html', context)
 
 
