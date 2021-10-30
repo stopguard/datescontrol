bbf1eb95618324135d7cef635b7821514ae4bdca
stopguard <stopguard@bk.ru>
2021-10-30 20:01:50 +0500

    edit authapp - add profile

--- authapp/forms.py
+++ authapp/forms.py
@@ -11,27 +11,22 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserCreateForm(UserCreationForm):
-    username = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'class': 'form-control',
-                                                                     'placeholder': 'username'}),
+    username = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'placeholder': 'username'}),
                                    required=True)
-    full_name = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'class': 'form-control',
-                                                                      'placeholder': 'Иванов Иван Иванович'}),
+    full_name = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'placeholder': 'Иванов Иван Иванович'}),
                                     required=True)
-    email = forms_lib.EmailField(widget=forms_lib.EmailInput(attrs={'class': 'form-control',
-                                                                    'placeholder': 'ivanov@mail.ru'}),
+    email = forms_lib.EmailField(widget=forms_lib.EmailInput(attrs={'placeholder': 'ivanov@mail.ru'}),
                                  required=True)
     photo = forms_lib.ImageField(widget=forms_lib.FileInput(attrs={'class': 'custom-file-input'}),
                                  required=False)
-    number = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'class': 'form-control',
-                                                                        'placeholder': '1234'}),
+    number = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'placeholder': '1234'}),
                                     required=True)
-    telegram_id = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'class': 'form-control',
-                                                                             'placeholder': '12345678'}),
+    telegram_id = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'placeholder': '12345678'}),
                                          required=False)
 
     class Meta:
         model = get_user_model()
-        fields = ('username', 'full_name', 'number', 'email', 'password1', 'password2')
+        fields = ('username', 'full_name', 'number', 'email', 'password1', 'password2', 'photo', 'telegram_id')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,29 +35,27 @@ class UserCreateForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-    username = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'class': 'form-control',
-                                                                     'placeholder': 'username',
+    username = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'placeholder': 'username',
                                                                      'readonly': True}))
-    full_name = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'class': 'form-control',
-                                                                      'placeholder': 'Иванов Иван Иванович'}),
+    full_name = forms_lib.CharField(widget=forms_lib.TextInput(attrs={'placeholder': 'Иванов Иван Иванович'}),
                                     required=True)
-    email = forms_lib.EmailField(widget=forms_lib.EmailInput(attrs={'class': 'form-control',
-                                                                    'placeholder': 'ivanov@mail.ru'}),
+    email = forms_lib.EmailField(widget=forms_lib.EmailInput(attrs={'placeholder': 'ivanov@mail.ru'}),
                                  required=True)
     photo = forms_lib.ImageField(widget=forms_lib.FileInput(attrs={'class': 'custom-file-input'}),
                                  required=False)
-    number = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'class': 'form-control',
-                                                                        'placeholder': '1234'}),
+    number = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'placeholder': '1234'}),
                                     required=True)
-    telegram_id = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'class': 'form-control',
-                                                                             'placeholder': '12345678'}),
+    telegram_id = forms_lib.IntegerField(widget=forms_lib.NumberInput(attrs={'placeholder': '12345678'}),
                                          required=False)
 
     class Meta:
         model = get_user_model()
-        fields = ('username', 'full_name', 'number', 'email', 'password')
+        fields = ('username', 'full_name', 'number', 'email', 'password', 'photo', 'telegram_id')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
+            if field_name == 'password':
+                field.widget = forms_lib.HiddenInput()
+                continue
             field.widget.attrs['class'] = f'form-control {field_name}'
--- authapp/views.py
+++ authapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm, UserCreateForm
+from authapp.forms import UserLoginForm, UserCreateForm, UserProfileForm
 
 
 def login(request):
@@ -51,3 +51,22 @@ def register(request):
                'today': today,
                }
     return render(request, 'authapp/register.html', context)
+
+
+def profile(request):
+    today = datetime.now() + timedelta(hours=5)
+    today = date(today.year, today.month, today.day)
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES,
+                               instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    context = {'page_title': 'профиль',
+               'form': form,
+               'today': today,
+               }
+    return render(request, 'authapp/profile.html', context)
